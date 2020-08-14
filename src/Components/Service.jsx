babda8dbbb2ef6f05149625c586d5a3353057eeb
Joanna Chien <joannachou713@gmail.com>
2020-08-14 23:25:33 +0800

    add mt-2

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -5,7 +5,7 @@ class Service extends Component {
     return (
       <div className="d-flex flex-column justify-content-center align-items-center">
         <img className='service-icon' src={this.props.imgPath} width="150" />
-        <span>{this.props.title}</span>
+        <span className='mt-2'>{this.props.title}</span>
       </div>
     );
   }
